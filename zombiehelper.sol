910d385e52b5d6b35f0f98b133ee772edf28f069
P. Doyle <patrick.doyle126@gmail.com>
2018-02-27 22:23:56 -0600

    adding contract modifier

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -4,6 +4,11 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+  modifier aboveLevel(uint _level, uint _zombieId) {
+    require(zombies[_zombieId].level >= _level);
+    _;
+  }
+
   // Start here
 
 }
