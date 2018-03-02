47d2b2526a19d58d771d7647adfcf31663f7fdbf
P. Doyle <patrick.doyle126@gmail.com>
2018-03-01 22:02:15 -0600

    working with 'payable' function

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -1,10 +1,23 @@
 pragma solidity ^0.4.19;
+
 import "./zombiefeeding.sol";
+
 contract ZombieHelper is ZombieFeeding {
+
+  uint levelUpFee = 0.001 ether;
 
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
+  }
+
+  // 1. Create withdraw function here
+
+  // 2. Create setLevelUpFee function here
+
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
   }
 
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
