316862b1010f846b115adbe18dcba3fe2482f5d2
P. Doyle <patrick.doyle126@gmail.com>
2018-03-05 20:44:49 -0600

    adding Attack Contract

---
+++ zombieattack.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.19;
+
+import "./zombiehelper.sol";
+
+contract ZombieBattle is ZombieHelper {
+  uint randNonce = 0;
+  // Create attackVictoryProbability here
+
+  function randMod(uint _modulus) internal returns(uint) {
+    randNonce++;
+    return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
+  }
+
+  // Create new function here
+}
