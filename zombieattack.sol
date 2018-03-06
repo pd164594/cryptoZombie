336189d600e5a500541b073c891f6a67e2020ea5
P. Doyle <patrick.doyle126@gmail.com>
2018-03-05 20:52:47 -0600

    add

--- zombieattack.sol
+++ zombieattack.sol
@@ -4,12 +4,13 @@ import "./zombiehelper.sol";
 
 contract ZombieBattle is ZombieHelper {
   uint randNonce = 0;
-  // Create attackVictoryProbability here
+  uint attackVictoryProbability = 70;
 
   function randMod(uint _modulus) internal returns(uint) {
     randNonce++;
     return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
   }
 
-  // Create new function here
+  function attack(uint _zombieId, uint _targetId) external {
+  }
 }
